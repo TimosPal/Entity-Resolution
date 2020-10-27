d1ae848d74e470f39c0fffb51387786c10ebd85a
TimosPal <timospale@gmail.com>
2020-10-27 21:22:12 +0200

    Added linked list

---
+++ includes/List.h
@@ -0,0 +1,27 @@
+#ifndef LIST_H
+#define LIST_H
+
+typedef struct Node {
+    struct Node* next;
+    void* value;
+}Node;
+
+typedef struct List {
+    Node* head;
+    Node* tail;
+
+    int size;
+}List;
+
+void List_Init(List* list); //Creates an empty list.
+
+void List_AddValue(List* list, void* value, int index); //Adds a node at index.
+void List_Append(List* list, void* value); //Appends a node at the end of the list.
+void List_Remove(List* list, int index); //Remove a node at index.
+void List_Destroy(List* list); //Destroy the list by freeing allocated nodes.
+void List_FreeValues(List list,void (*freeMethod)(void*)); //Frees allocated memory for the node's values. Does NOT free the nodes.
+void List_DefaultFreeMethod(void* value); //Can be used in List_Free values for primitive types or structs containing primitive types.
+
+Node* List_GetNode(List list, int index); //Get Node at index.
+
+#endif  
---
+++ modules/LinkedList/List.c
@@ -0,0 +1,96 @@
+#include "List.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void List_Init(List* list) {
+	list->head = NULL;
+	list->tail = NULL;
+	list->size = 0;
+}
+
+void List_Destroy(List* list) {
+	while (list->head != NULL)
+		List_Remove(list, 0);
+}
+
+void List_AddValue(List* list, void* value, int index) {
+	Node* newNode = malloc(sizeof(Node));
+	newNode->value = value;
+	list->size++;
+
+	if (index == 0) {
+		if (list->head == NULL)
+			list->tail = newNode;
+
+		newNode->next = list->head;
+		list->head = newNode;
+	} else {
+		Node* temp = list->head;
+		for (int i = 0; i < index - 1; i++)
+			temp = temp->next;
+
+		newNode->next = temp->next;
+		temp->next = newNode;
+
+		if (newNode->next == NULL)
+			list->tail = newNode;
+	}
+}
+
+void List_Append(List* list, void* value) {
+	Node* newNode = malloc(sizeof(Node));
+	newNode->value = value;
+	newNode->next = NULL;
+
+	if (list->head == NULL)
+		list->head = newNode;
+	else
+		list->tail->next = newNode;
+	list->tail = newNode;
+	list->size++;
+}
+
+void List_Remove(List* list, int index) {
+	Node* temp = list->head;
+	list->size--;
+
+	if (index == 0) {
+		list->head = list->head->next;
+		free(temp);
+
+		if (list->head == NULL)
+			list->tail = NULL;
+	} else {
+		for (int i = 0; i < index - 1; i++)
+			temp = temp->next;
+
+		Node* old = temp->next;
+		temp->next = old->next;
+		free(old);
+
+		if (temp->next == NULL) {
+			list->tail = temp;
+		}
+	}
+}
+
+Node* List_GetNode(List list, int index) {
+	Node* temp = list.head;
+	for (int i = 0; i < index; i++)
+		temp = temp->next;
+
+	return temp;
+}
+
+void List_FreeValues(List list,void (*subFree)(void*)){
+	Node* currNode = list.head;
+	while(currNode != NULL){
+		subFree(currNode->value);
+		currNode = currNode->next;
+	}
+}
+
+void List_DefaultFreeMethod(void* value){
+	free(value);
+}
