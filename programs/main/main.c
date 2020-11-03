5500f590fa080b1fdd17815ac746a02b45246767
TimosPal <timospale@gmail.com>
2020-11-03 19:49:36 +0200

    Put main's code into more organized functions

--- programs/main/main.c
+++ programs/main/main.c
@@ -13,10 +13,12 @@
 
 #include "Item.h"
 
-/* It is assumed that the json and csv files MUST be correct in format and values
- * so no extra error checking is done on said files , they are parsed right away. */
+/* It is assumed that the json and csv files have proper formatting and appropriate values ,
+ * so no extra error checking is done. */
 
 int CalculateBucketSize(char* websitesFolderPath){
+    /* Counts the number of files in the folders to estimate a good size for the hash */
+
     int bucketSize = 0;
     List websiteFolders;
     IF_ERROR_MSG(!GetFolderItems(websitesFolderPath, &websiteFolders), "failed to open/close base folder")
@@ -40,39 +42,32 @@ int CalculateBucketSize(char* websitesFolderPath){
     return bucketSize;
 }
 
-int main(int argc, char* argv[]){
-    /* --- Arguments --------------------------------------------------------------------------*/
-
+void ParseArgs(int argc, char* argv[], char **websitesFolderPath,char **dataSetWPath,int* bucketSize){
     // Get the flags from argv.
     // -f should contain the path to the folder containing the websites folders.
-    char *websitesFolderPath;
-    IF_ERROR_MSG(!FindArgAfterFlag(argv, argc, "-f", &websitesFolderPath), "arg -f is missing or has no value")
+    IF_ERROR_MSG(!FindArgAfterFlag(argv, argc, "-f", websitesFolderPath), "arg -f is missing or has no value")
+    IF_ERROR_MSG(!FindArgAfterFlag(argv, argc, "-w", dataSetWPath), "arg -w is missing or has no value")
 
-    char *dataSetWPath;
-    IF_ERROR_MSG(!FindArgAfterFlag(argv, argc, "-w", &dataSetWPath), "arg -w is missing or has no value")
-
-    // -b is the bucketsize
-    int bucketSize;
+    // -b is the bucketsize.
     char *bucketSizeStr;
     // If -b is not provided we estimate a good size by counting the number of files.
     // We know each file is equivalent to hash key.
     if(FindArgAfterFlag(argv, argc, "-b", &bucketSizeStr)) {
-        IF_ERROR_MSG(!StringToInt(bucketSizeStr, &bucketSize), "Bucket Size should be a number")
+        IF_ERROR_MSG(!StringToInt(bucketSizeStr, bucketSize), "Bucket Size should be a number")
     }else{
         // We estimated the size based on the files number.
-        bucketSize = CalculateBucketSize(websitesFolderPath);
+        *bucketSize = CalculateBucketSize(*websitesFolderPath);
         //printf("------------- %d -------------\n",bucketSize);
     }
+}
 
-    /* --- Json / Clique_adds -----------------------------------------------------------------*/
+void HandleData_X(char* websitesFolderPath,int bucketSize,CliqueGroup* cliqueGroup){
+    /* Reads json files inside the given folder path and adds them to a cliqueGroup data structure
+     * for further processing. */
 
     // Open folder from -f (should contain more folder with names of websites)
     List websiteFolders;
     IF_ERROR_MSG(!GetFolderItems(websitesFolderPath, &websiteFolders), "failed to open/close base folder")
-
-    /* Create CliqueGroup structure (complete structure) */
-    CliqueGroup cliqueGroup;
-    CliqueGroup_Init(&cliqueGroup, bucketSize, RSHash, StringCmp);
 
     // Open each website folder.
     Node* currWebsiteFolder = websiteFolders.head;
@@ -87,7 +82,7 @@ int main(int argc, char* argv[]){
         /* Create Nodes from the list of Json file names */
         Node* currItem = currItems.head;
         while(currItem != NULL){
-            /* The json relative file path 
+            /* The json relative file path
             i.e ../../Datasets/camera_specs/2013_camera_specs/www.walmart.com/767.json */
             char jsonFilePath[BUFFER_SIZE];
             sprintf(jsonFilePath,"%s/%s",websitePath,(char*)(currItem->value));
@@ -99,8 +94,7 @@ int main(int argc, char* argv[]){
 
             /* Create item and insert into items list */
             Item* item = Item_Create(itemID, GetJsonPairs(jsonFilePath));
-            /* TODO: make bucket size dynamic */
-            CliqueGroup_Add(&cliqueGroup, item->id, strlen(itemID)+1, item);
+            CliqueGroup_Add(cliqueGroup, item->id, (int)strlen(itemID)+1, item);
 
             currItem = currItem->next;
         }
@@ -111,10 +105,17 @@ int main(int argc, char* argv[]){
         List_Destroy(&currItems);
     }
 
-    /* --- CSV / Clique_Updates ---------------------------------------------------------------*/
+    List_FreeValues(websiteFolders,free);
+    List_Destroy(&websiteFolders);
+}
 
-    // Update cliqueGroup with dataSetW.
-    // We apply the simple logic that for items a,b,c : if a == b and b == c then a == c.
+void HandleData_W(char* dataSetWPath,CliqueGroup* cliqueGroup){
+    /* Update cliqueGroup with dataSetW.
+     * We apply the simple logic that for items a,b,c : if a == b and b == c then a == c.
+     * dataSetW contains values of the following format :
+     * idA , idB , 0/1
+     * 1 = idA and idB are similar items and the cliqueGroup should be updated. */
+
     FILE* dataSetFile = fopen(dataSetWPath, "r");
     IF_ERROR_MSG(dataSetFile == NULL, "-w file not found")
 
@@ -131,7 +132,7 @@ int main(int argc, char* argv[]){
 
         // If the 2 items are similar we merge the cliques.
         if(similarity == 1) {
-            CliqueGroup_Update(&cliqueGroup, id1, (int) strlen(id1) + 1, id2, (int) strlen(id2) + 1);
+            CliqueGroup_Update(cliqueGroup, id1, (int) strlen(id1) + 1, id2, (int) strlen(id2) + 1);
         }
 
         List_FreeValues(values, free);
@@ -139,16 +140,33 @@ int main(int argc, char* argv[]){
     }
 
     fclose(dataSetFile);
+}
 
-    /* Print Output Results */
+int main(int argc, char* argv[]){
+    /* --- Arguments --------------------------------------------------------------------------*/
+
+    char *websitesFolderPath , *dataSetWPath;
+    int bucketSize;
+    ParseArgs(argc, argv, &websitesFolderPath, &dataSetWPath, &bucketSize);
+
+    /* --- Reads Json files and adds them to the clique ---------------------------------------*/
+
+    CliqueGroup cliqueGroup;
+    CliqueGroup_Init(&cliqueGroup, bucketSize, RSHash, StringCmp);
+    HandleData_X(websitesFolderPath,bucketSize,&cliqueGroup);
+
+    /* --- Reads CSV files and updates the cliqueGroups ---------------------------------------*/
+
+    HandleData_W(dataSetWPath,&cliqueGroup);
+
+    /* --- Print results ----------------------------------------------------------------------*/
+
     CliqueGroup_PrintIdentical(&cliqueGroup, Item_Print);
 
-    /* Deletes items ONLY(within cliques list(which has more lists in it)) */
+    /* --- Clean up ---------------------------------------------------------------------------*/
+
     CliqueGroup_FreeValues(cliqueGroup, Item_Free);
     CliqueGroup_Destroy(cliqueGroup);
-
-    List_FreeValues(websiteFolders,free);
-    List_Destroy(&websiteFolders);
 
     return 0;
 }
