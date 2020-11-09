06292c25260d6b49eaa46ef1cc4b48e9bde7d598
TimosPal <timospale@gmail.com>
2020-11-09 22:53:47 +0200

    fixed some mem leaks in new unit test

--- tests/StringUtil_Test.c
+++ tests/StringUtil_Test.c
@@ -17,6 +17,8 @@ void StringUtil_Test_StringSplit(){
     TEST_ASSERT(*(char*)l.head->next->value == '2');
     TEST_ASSERT(*(char*)l.head->next->next->value == '3');
     TEST_ASSERT(*(char*)l.head->next->next->next->value == '4');
+    List_FreeValues(l,free);
+    List_Destroy(&l);
 }
 
 TEST_LIST = {
